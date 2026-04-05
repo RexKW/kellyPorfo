39bcdb8413f57223ede1eb35969d97ccc2913eed
Rex Kenny Wirasantoso <153161558+RexKW@users.noreply.github.com>
2026-04-06 00:13:20 +0700

    added bakery

--- app/main-work/page.tsx
+++ app/main-work/page.tsx
@@ -8,7 +8,7 @@ export default function MainWork() {
         <div className="flex-flex-col justify-center items-center relative z-1">
           <Image width={1200} height={600} src={'/mainWork/donutBackdrop.png'} alt="" className="w-screen h-auto" />
           <div className="bg-[#f5b0c7] w-full h-[45vh] lg:h-[50vh] justify-center items-center relative">
-            <img src="/mainWork/monelieGreen.svg" className="absolute left-[50%] translate-x-[-50%] z-2 w-[50%] translate-y-[-75%]" alt="" />
+            <img src="/mainWork/monelie/monelieGreen.svg" className="absolute left-[50%] translate-x-[-50%] z-2 w-[50%] translate-y-[-75%]" alt="" />
             <div className="bg-[#f5b0c7] w-full h-full blur-lg">
 
             </div>
@@ -22,7 +22,7 @@ export default function MainWork() {
               membuat Monelie semakin menyatu dengan tema</p>
           </div>
         </div>
-        
+
       </div>
       <div className="flex flex-col bg-[#00998b]/40 justify-center items-center relative h-screen w-full">
         <div className="bg-linear-to-b from-[#f5b0c7] absolute top-0 to-transparent h-[25%] w-full">
@@ -31,30 +31,43 @@ export default function MainWork() {
       </div>
       <div className="flex flex-col bg-white justify-center items-center relative h-[200px] lg:h-screen w-full">
         <div className="flex flex-row px-10  w-full h-[100px] lg:h-full">
-          <div className="basis-1/6 h-full flex items-start"><img src="/mainWork/monelieAsset1.svg" className="w-[75%]" alt="" /></div>
-            <div className="basis-1/6 h-full flex items-end"><img src="/mainWork/monelieAsset2.svg" className="w-[75%]" alt="" /></div>
-            <div className="basis-1/6 h-full flex items-start"><img src="/mainWork/monelieAsset3.svg" className="w-[75%]" alt="" /></div>
-            <div className="basis-1/6 h-full flex items-end"><img src="/mainWork/monelieAsset4.svg" className="w-[75%]" alt="" /></div>
-            <div className="basis-1/6 h-full flex items-start"><img src="/mainWork/monelieAsset5.svg" className="w-[75%]" alt="" /></div>
-            <div className="basis-1/6 h-full flex items-end"><img src="/mainWork/monelieAsset6.svg" className="w-[75%]" alt="" /></div>
+          <div className="basis-1/6 h-full flex items-start"><img src="/mainWork/monelie/monelieAsset1.svg" className="w-[75%]" alt="" /></div>
+          <div className="basis-1/6 h-full flex items-end"><img src="/mainWork/monelie/monelieAsset2.svg" className="w-[75%]" alt="" /></div>
+          <div className="basis-1/6 h-full flex items-start"><img src="/mainWork/monelie/monelieAsset3.svg" className="w-[75%]" alt="" /></div>
+          <div className="basis-1/6 h-full flex items-end"><img src="/mainWork/monelie/monelieAsset4.svg" className="w-[75%]" alt="" /></div>
+          <div className="basis-1/6 h-full flex items-start"><img src="/mainWork/monelie/monelieAsset5.svg" className="w-[75%]" alt="" /></div>
+          <div className="basis-1/6 h-full flex items-end"><img src="/mainWork/monelie/monelieAsset6.svg" className="w-[75%]" alt="" /></div>
         </div>
         <div className="w-full h-[100px] lg:h-[50vh] flex">
-            <div className="bg-[#82c1b9] basis-1/5 h-full"></div>
-            <div className="bg-[#c8e8e5] basis-1/5 h-full"></div>
-            <div className="bg-[#f2f9fa] basis-1/5 h-full"></div>
-            <div className="bg-[#fad2de] basis-1/5 h-full"></div>
-            <div className="bg-[#f3a3bf] basis-1/5 h-full"></div>
+          <div className="bg-[#82c1b9] basis-1/5 h-full"></div>
+          <div className="bg-[#c8e8e5] basis-1/5 h-full"></div>
+          <div className="bg-[#f2f9fa] basis-1/5 h-full"></div>
+          <div className="bg-[#fad2de] basis-1/5 h-full"></div>
+          <div className="bg-[#f3a3bf] basis-1/5 h-full"></div>
         </div>
       </div>
       <div className="flex relative flex-col h-screen bg-white overflow-hidden">
         <div className="bg-[#fad2de]/25 h-[50vh] w-full blur-md absolute bottom-0"></div>
-        <img src="/mainWork/monelieBanners.png" className="w-[60%] absolute top-[50%] translate-y-[-50%] left-25" alt="" />
-        <img src="/mainWork/bgMonelieBanner.png" className="w-screen h-full absolute top-0  left-0" alt="" />
-        <img src="/mainWork/cornerAssetMonelie.svg" className="w-[25%] h-auto absolute top-0 right-0" alt="" />
-        <img src="/mainWork/bottomCornerMonelie.svg" alt="" className="w-[20%] absolute bottom-[-5%] right-[-5%]" />
+        <img src="/mainWork/monelie/monelieBanners.png" className="w-[60%] absolute top-[50%] translate-y-[-50%] left-25" alt="" />
+        <img src="/mainWork/monelie/bgMonelieBanner.png" className="w-screen h-full absolute top-0  left-0" alt="" />
+        <img src="/mainWork/monelie/cornerAssetMonelie.svg" className="w-[25%] h-auto absolute top-0 right-0" alt="" />
+        <img src="/mainWork/monelie/bottomCornerMonelie.svg" alt="" className="w-[20%] absolute bottom-[-5%] right-[-5%]" />
       </div>
       <div className="flex flex-col justify-center items-center h-screen">
-          <img src="/mainWork/monelieStand.png" className="w-[50%]" alt="" />
+        <img src="/mainWork/monelie/monelieStand.png" className="w-[50%]" alt="" />
+      </div>
+      <div className="flex-flex-col overflow-hidden justify-center items-center relative z-1">
+        <Image width={1200} height={600} src={'/mainWork/bakery/JoBakeryBG.png'} alt="" className="w-screen h-auto" />
+        <div className="bg-white w-full h-[45vh] lg:h-[50vh] justify-center items-center relative">
+          <img src="/mainWork/bakery/joBakeryLogo.svg" className="absolute left-[50%] translate-x-[-50%] z-2 w-[20%] translate-y-[-75%]" alt="" />
+          <p className="text-start text-[#523114] top-10 w-[50%] lg:top-[25%] absolute text-2xl z-2 px-5 lg:px-10">Jonathan's Bakery adalah toko roti asal NTT
+            design ini dibuat berdasarkan pengembangan
+            konsep dari Jonathan's Bakery dengan menambahkan
+            sentuhan parisian bakery. Rebranding ini bertujuan
+            untuk meningkatkan standard di NTT</p>
+            <img src="/mainWork/bakery/asset2Bakery.svg" className="w-60 bottom-0 right-0 absolute" alt="" />
+          <img src="/mainWork/bakery/asset1Bakery.svg" className="w-50 bottom-[-5%] right-[-5%] absolute" alt="" />
+        </div>
       </div>
 
     </div>
